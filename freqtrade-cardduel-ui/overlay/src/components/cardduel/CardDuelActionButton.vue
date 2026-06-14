21ff2f1bf9d3f9a317c58ea45a23515a89fdb559
a44056283-maker <a44056283@gmail.com>
2026-06-14 22:08:51 +0800

    feat(cardduel): add action button component

---
+++ freqtrade-cardduel-ui/overlay/src/components/cardduel/CardDuelActionButton.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+defineProps<{ danger?: boolean }>();
+</script>
+
+<template>
+  <button class="cardduel-button px-4 py-2" :class="danger ? 'cardduel-button-danger' : ''">
+    <slot />
+  </button>
+</template>
