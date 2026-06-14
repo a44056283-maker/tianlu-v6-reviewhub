3871626c95433a29c38b89abec54aa702aef0225
a44056283-maker <a44056283@gmail.com>
2026-06-14 22:08:32 +0800

    feat(cardduel): add data table component

---
+++ freqtrade-cardduel-ui/overlay/src/components/cardduel/CardDuelDataTable.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+defineProps<{
+  headers: string[];
+  rows: Array<Array<string | number>>;
+}>();
+</script>
+
+<template>
+  <div class="cardduel-card overflow-x-auto p-4">
+    <table class="cardduel-table min-w-full">
+      <thead>
+        <tr>
+          <th v-for="h in headers" :key="h">
+            {{ h }}
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(row, i) in rows" :key="i">
+          <td v-for="(cell, j) in row" :key="j">
+            {{ cell }}
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
